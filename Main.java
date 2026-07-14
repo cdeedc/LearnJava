c701f641a48a33e9c221bbb857f28076c176ea73
cdeedc <ericdimitriecismaru@gmail.com>
2026-07-14 18:42:59 +0300

    Update

---
+++ Main.java
@@ -0,0 +1,33 @@
+package LearnJava;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	    Elev e = new Elev();
+	    Elev e2 = new Elev("Ion");
+	    e2.afisare();
+	}
+}
+
+class Elev {
+    public String nume;
+    public String prenume;
+    public double medieAnuala;
+    
+    public Elev() {
+        
+    }
+    
+    public Elev(String n) {
+        nume = n;
+    }
+
+    public Elev(String n, String p) {
+        nume = n;
+        prenume = p;
+    }
+    
+    public void afisare() {
+        System.out.println(nume);
+    }
+}
