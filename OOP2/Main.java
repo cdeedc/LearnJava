93d3abb227e5dd082d82f93b9f86b04ccf040963
cdeedc <ericdimitriecismaru@gmail.com>
2026-07-15 19:39:57 +0300

    Update

--- OOP2/Main.java
+++ OOP2/Main.java
@@ -3,45 +3,45 @@ public class Main {
     public static void main(String[] args) {
         // Persoana
         Persoana p = new Persoana("Ion", "Ion");
-        p.afisare();
+        // p.afisare();
 
         // Elev
         Elev e = new Elev();
-        e.afisare();
+        // e.afisare();
         e.nume = "Zamfir";
         e.prenume = "Dan";
         e.setMedieAnuala(9.85);
 
         Elev e1 = new Elev("Ion");
-        e1.afisare();
+        // e1.afisare();
         e1.nume = "Pop";
         e1.prenume = "Mihai";
         e1.setMedieAnuala(9.53d);
 
         Elev e2 = new Elev("Popescu", "Alex", 9.95d);
-        e2.afisareMedieAnuala();
-        System.out.println(e2.getMedieAnuala() + "\n");
+        // e2.afisareMedieAnuala();
+        // System.out.println(e2.getMedieAnuala() + "\n");
 
         // Profesor
         Profesor prof = new Profesor("Popescu");
-        System.out.println(prof.nume + "\n");
+        // System.out.println(prof.nume + "\n");
         prof.nume = "Costel";
         prof.setSalariu(10050.d);
 
         Profesor prof1 = new Profesor("Stefan", "Alex", 10000.0d);
-        System.out.println(prof1.nume);
-        System.out.println(prof1.prenume);
-        prof1.afisareSalariu();
-        System.out.println();
+        // System.out.println(prof1.nume);
+        // System.out.println(prof1.prenume);
+        // prof1.afisareSalariu();
+        // System.out.println();
 
         // Clasa
         Elev[] elevi = { e, e1, e2 };
         Clasa clasa = new Clasa(prof1, elevi, Nivel.XI, Litera.C);
-        clasa.afisare();
+        // clasa.afisare();
 
         Clasa clasa2 = new Clasa(prof, elevi, Nivel.IX, Litera.A);
-        clasa2.afisare();
-        System.out.println();
+        // clasa2.afisare();
+        // System.out.println();
 
         // Scoala
         Clasa[] clase = new Clasa[16];
@@ -56,6 +56,14 @@ public class Main {
         Profesor[] profs = { prof, prof1 };
         String adr = "Str. Egalitatii";
         Scoala scoala = new Scoala(profs, clase, prof, adr, "Colegiul National \"Zinca Golescu\"");
-        scoala.afisare();
+        // scoala.afisare();
+
+        // Oras
+        adr = "Str. Vasile Parvan";
+        Scoala scoala1 = new Scoala(profs, clase, prof, adr, "Colegiul National \"Alexandru Odobescu\"");
+
+        Scoala[] scoli = { scoala, scoala1 };
+        Oras oras = new Oras(scoli, "Pitesti");
+        oras.afisare();
     }
 }
--- OOP2/Oras.java
+++ OOP2/Oras.java
@@ -2,7 +2,20 @@ public class Oras {
     public Scoala[] scoli;
     public String nume;
 
-    public Oras() {
+    public Oras(Scoala[] scl, String n) {
+        scoli = scl;
+        nume = n;
+    }
 
+    public void afisare() {
+        System.out.println("==============================================");
+        System.out.println(">>>>>>>>>> Orasul " + nume + "<<<<<<<<<<<<<");
+
+        System.out.println("Scoli componente:\n");
+        for(int i = 0; i < scoli.length; i++) {
+            scoli[i].afisare();
+        }
+
+        System.out.println("==============================================");
     }
 }
