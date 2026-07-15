134e632155e475ab84d5b978fded05a6a88e52b1
CismaruEricDimitrie <ericdimitriecismaru@gmail.com>
2026-07-15 08:47:29 +0300

    Updated structure of Java course

---
+++ Introduction/sum00.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class sum00 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt(),
+            y = sc.nextInt();
+
+        System.out.println(x + y);
+        sc.close();
+    }
+}
